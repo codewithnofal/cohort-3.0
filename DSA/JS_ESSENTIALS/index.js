faf160aea4aed2f7eaf2efb17caefe8c9533d230
Nofal Ali <nofilkachhot@gmail.com>
2026-06-01 12:16:10 +0530

    1st q for test

--- DSA/JS_ESSENTIALS/index.js
+++ DSA/JS_ESSENTIALS/index.js
@@ -1,1 +1,12 @@
 // ------------------------------here is a some programs for the js -------------------------------------
+
+// 1. sum of two numbers.
+
+function sumOftwo() {
+  let a = 10;
+  let b = 20;
+
+  let c = a + b;
+  console.log(c);
+}
+sumOftwo()
