3fb4245600815f40e887a9e14d9e2161d042e2d1
Nofal Ali <nofilkachhot@gmail.com>
2026-06-06 22:13:54 +0530

    most frequent count

--- JS/practiceSession/index.js
+++ JS/practiceSession/index.js
@@ -18,6 +18,37 @@ function findTotalAndEverage() {
   }, 0);
   let average = total / marks.length;
   console.log(average);
-//   console.log(total);
+  //   console.log(total);
 }
-findTotalAndEverage();
+// findTotalAndEverage();
+
+// 3.
+
+function mostFrequencyCount() {
+  let nums = [1, 2, 3, 2, 4, 2, 5, 1, 1, 1];
+
+  let count = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+
+    if (count[num]) {
+      count[num] = count[num] + 1;
+    } else {
+      count[num] = 1;
+    }
+  }
+  // console.log(count);
+
+  let frequency = 0;
+  let mostFrequent;
+
+  for (let key in count) {
+    if (count[key] > frequency) {
+      frequency = count[key];
+      mostFrequent = key;
+    }
+  }
+  console.log(mostFrequent);
+}
+// mostFrequencyCount();
