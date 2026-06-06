b4be58030dec9a63a876321671624f30180bb02d
Nofal Ali <nofilkachhot@gmail.com>
2026-06-06 12:11:31 +0530

    solve a problem without extra space

--- DSA/JS_ESSENTIALS/index.js
+++ DSA/JS_ESSENTIALS/index.js
@@ -713,3 +713,29 @@ function mergeSortedArraywithextraSpace() {
   console.log(numsCopy);
 }
 // mergeSortedArray();
+
+// 2nd uproach without extra space
+
+function mergeSortedArraywithoutExtraSpace() {
+  let nums1 = [1, 2, 3];
+  let m = 3;
+  let nums2 = [2, 5, 6];
+  let n = 3;
+
+  let p1 = m - 1;
+  let p2 = n - 1;
+
+  for(let i=m+n-1; i>=0; i--){
+    if(p2 < 0) break;
+    if(p1 >=0 && nums1[p1] > nums2[p2]){
+      nums1[i] = nums1[p1]
+      p1--
+    }
+    else{
+      nums1[i] = nums2[p2]
+      p2--
+    }
+  }
+  console.log(nums1)
+}
+mergeSortedArraywithoutExtraSpace();
