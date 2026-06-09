6ada2979a05d425a9dd7b1e1f37798fb4d302bf5
Nofal Ali <nofilkachhot@gmail.com>
2026-06-09 17:15:20 +0530

    solve some hard dsa questions of arrays

--- JS/practiceSheet1/index.js
+++ JS/practiceSheet1/index.js
@@ -122,7 +122,115 @@ function findFirstAdult() {
 
 function positiveNumberCheck() {
   let nums = [5, 8, 10, 3];
-  let positiveNum = nums.every((n) => n > 0)
-  console.log(positiveNum)
+  let positiveNum = nums.every((n) => n > 0);
+  console.log(positiveNum);
 }
 // positiveNumberCheck();
+
+// Question 15 — Most Frequent Number.
+
+function mostFrequentNum() {
+  let numbers = [1, 2, 3, 2, 4, 2, 5, 1, 1, 1];
+  let count = {};
+
+  for (let i = 0; i < numbers.length; i++) {
+    let num = numbers[i];
+    if (count[num]) {
+      count[num] = count[num] + 1;
+    } else {
+      count[num] = 1;
+    }
+  }
+  console.log(count);
+}
+// mostFrequentNum();
+
+// Question 16 — Second Largest Number.
+
+function seconLargestNum() {
+  let nums = [10, 50, 20, 80, 40];
+  let largest = -Infinity;
+  let secLargest = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > largest) {
+      secLargest = largest;
+      largest = nums[i];
+    } else if (nums[i] > secLargest) {
+      secLargest = nums[i];
+    }
+  }
+  console.log(secLargest);
+}
+// seconLargestNum();
+
+// Question 17 — Remove Duplicates
+
+function removeDuplicates() {
+  let ids = [1, 2, 2, 3, 4, 4, 5, 5];
+
+  let x = 0;
+
+  for (let i = 0; i < ids.length; i++) {
+    if (ids[i] > ids[x]) {
+      x++;
+      ids[x] = ids[i];
+    }
+  }
+  let newArr = ids.slice(0, x + 1);
+  console.log(newArr);
+}
+// removeDuplicates();
+
+// Question 18 — Longest Word.
+
+function longestWord() {
+  let words = ["JavaScript", "HTML", "CSS", "Programming"];
+  let longestW = 0;
+  let longestword;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestW) {
+      longestW = words[i].length;
+      longestword = words[i];
+    }
+  }
+  console.log(longestword);
+  // console.log(longestW);
+}
+// longestWord();
+
+// Question 19 — Rotate Array Right.
+
+function rotateArrarRight() {
+  let nums = [1, 2, 3, 4, 5];
+  nums.pop();
+  nums.unshift(5);
+  console.log(nums);
+}
+// rotateArrarRight();
+
+// Question 20 — Best Selling Product.
+
+function bestSellingProducts() {
+  let sales = ["Mouse", "Keyboard", "Mouse", "Laptop", "Mouse", "Keyboard"];
+  let count = {};
+
+  for (let i = 0; i < sales.length; i++) {
+    let word = sales[i];
+    if (count[word]) {
+      count[word] = count[word] + 1;
+    } else {
+      count[word] = 1;
+    }
+  }
+  let maxCount = 0;
+  let pName;
+  for (let key in count) {
+    if (count[key] > maxCount) {
+      maxCount = count[key];
+      pName = key;
+    }
+  }
+  console.log(pName)
+}
+bestSellingProducts();
