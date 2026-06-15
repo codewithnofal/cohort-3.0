e2a921b7326c63bf8280bd499d58099f411bf46b
Nofal Ali <nofilkachhot@gmail.com>
2026-06-15 12:24:02 +0530

    solve mid level of q

--- JS/practiceSheet2/index.js
+++ JS/practiceSheet2/index.js
@@ -16,4 +16,41 @@ function introduce(city, country) {
 
 // introduce.apply(person,["Ahmedabad", "India"])
 
+// 🟡 Moderate Level (Q5 - Q7).
+
+// Question 5 — Fix Lost.
+
+const user = {
+  name: "Ritik",
+  greet() {
+    console.log(this.name);
+  },
+};
+
+// const fn = user.greet.bind(user)
+// fn()
+
+// Question 6 — Create an Inheritance Chain.
+
+// const animal = { eats: true };
+
+// const dog = Object.create(animal);
+
+// console.log(dog.eats)
+
+// Question 7 — Prototype Method.
+
+function Person(name) {
+  this.name = name;
+}
+
+// Person.prototype.greet = function() {
+//     console.log("Welcome", this.name)
+// }
+
+// let p1 = new Person("Nofal")
+// p1.greet()
+
+// 🔴 Hard Level (Q8 - Q10).
+
 
