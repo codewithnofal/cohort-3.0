541fda2ba24242e1d23c508ad44c09c0b42850be
Nofal Ali <nofilkachhot@gmail.com>
2026-06-15 12:05:29 +0530

    solve easy level of q

--- JS/practiceSheet2/index.js
+++ JS/practiceSheet2/index.js
@@ -1,4 +1,19 @@
+// Question 3 — call().
+
 function introduce() {
   console.log(this.name);
 }
-const person = { name: "Ritik" };
+// const person = { name: "Ritik" };
+
+// introduce.call(person)
+
+// Question 4 — apply().
+
+function introduce(city, country) {
+  console.log(`${this.name} from ${city}, ${country}`);
+}
+// const person = { name: "Ritik" };
+
+// introduce.apply(person,["Ahmedabad", "India"])
+
+
