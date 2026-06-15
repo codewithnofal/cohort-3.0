68871d204e1557eb12fbc7012f46fde9bc26720f
Nofal Ali <nofilkachhot@gmail.com>
2026-06-15 15:57:46 +0530

    completed hard q of oops

--- JS/practiceSheet2/index.js
+++ JS/practiceSheet2/index.js
@@ -53,4 +53,83 @@ function Person(name) {
 
 // 🔴 Hard Level (Q8 - Q10).
 
+// Question 8 — Student Class System.
 
+class Student {
+  constructor(name, marks) {
+    this.name = name;
+    this.marks = marks;
+  }
+
+  getGrade() {
+    let grade;
+    if (this.marks >= 91 && this.marks <= 100) {
+      grade = "A";
+    } else if (this.marks >= 75 && this.marks <= 90) {
+      grade = "B";
+    } else if (this.marks >= 61 && this.marks <= 74) {
+      grade = "C";
+    } else {
+      grade = "F";
+    }
+    console.log(grade);
+  }
+}
+// let st1 = new Student("Nofal", 93);
+// st1.getGrade();
+
+// Question 9 — Employee Inheritance.
+
+// class Employee {
+//   constructor(name, salary) {
+//     this.name = name;
+//     this.salary = salary;
+//   }
+//   work() {
+//     console.log(
+//       `${this.name} is working here and his salary is ${this.salary}`,
+//     );
+//   }
+// }
+
+// class Developer extends Employee {
+//   constructor(name, salary) {
+//     super(name, salary);
+//   }
+//   code() {
+//     console.log(this.name);
+//   }
+// }
+
+// let dev1 = new Developer("Nofal", 100000);
+// dev1.work();
+
+// Question 10 — Bank Account (InterviewLevel).
+
+class BankAccount {
+  balance = 0;
+
+  deposite(amount) {
+    return (this.#balance += amount);
+  }
+  withdraw(amount) {
+    return (this.#balance -= amount);
+  }
+  getBalance() {
+    return console.log(this.#balance);
+  }
+}
+
+// let acc1 = new BankAccount();
+
+// acc1.deposite(1000)
+// acc1.withdraw(300)
+
+// acc1.getBalance()
+
+
+
+// Library Management System
+class Book {
+
+}
