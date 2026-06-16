037f2d599a9176a5b70e1543146029ca9a65b82c
Nofal Ali <nofilkachhot@gmail.com>
2026-06-16 23:20:31 +0530

    updated

--- JS/RANDOM_PRACTICE/index.js
+++ JS/RANDOM_PRACTICE/index.js
@@ -130,24 +130,26 @@
 
 // 2nd uproach optimized
 
-let nums1 = [1, 2, 3];
-let nums2 = [2, 5, 6];
+// let nums1 = [1, 2, 3];
+// let nums2 = [2, 5, 6];
 
-let m = 3;
-let n = 3;
+// let m = 3;
+// let n = 3;
 
-let p1 = m - 1;
-let p2 = n - 1;
+// let p1 = m - 1;
+// let p2 = n - 1;
 
-for (let i = m + n - 1; i >= 0; i--) {
-  if (p2 < 0) break;
+// for (let i = m + n - 1; i >= 0; i--) {
+//   if (p2 < 0) break;
 
-  if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-    nums1[i] = nums1[p1];
-    p1--;
-  } else {
-    nums1[i] = nums2[p2];
-    p2--;
-  }
-}
-console.log(nums1)
+//   if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+//     nums1[i] = nums1[p1];
+//     p1--;
+//   } else {
+//     nums1[i] = nums2[p2];
+//     p2--;
+//   }
+// }
+// console.log(nums1)
+
+// 
