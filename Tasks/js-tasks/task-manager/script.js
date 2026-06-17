f21f7bbc1505de759c77946dd1ba6eada41cd322
Nofal Ali <nofilkachhot@gmail.com>
2026-06-17 22:07:29 +0530

    completed a 90% css part now go for the js

--- Tasks/js-tasks/task-manager/script.js
+++ Tasks/js-tasks/task-manager/script.js
@@ -1,12 +1,18 @@
 const createBtn = document.querySelector(".create-btn");
 const closeBtn = document.querySelector(".close");
-const form = document.querySelector(".container");
-console.log(createBtn);
-console.log(form);
+const form = document.querySelector("form");
+
+let Tasks = [];
 
 createBtn.addEventListener("click", () => {
   form.style.display = "flex";
 });
 closeBtn.addEventListener("click", () => {
   form.style.display = "none";
+});
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+  let name = e.target[0].value;
+  console.log(name);
 });
