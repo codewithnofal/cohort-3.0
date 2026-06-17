3bf2f493ae037f7c22155b84aee5a6e29a7e7976
Nofal Ali <nofilkachhot@gmail.com>
2026-06-17 10:46:48 +0530

    solve a move zeros q

--- JS/RANDOM_PRACTICE/index.js
+++ JS/RANDOM_PRACTICE/index.js
@@ -152,4 +152,20 @@
 // }
 // console.log(nums1)
 
-// 
+// Move zeros
+
+let arr = [1, 0, 3, 0, 0, 5, 8, 10, 0, 0];
+
+let x = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] !== 0) {
+    arr[x] = arr[i];
+    x++;
+  }
+}
+for (let i = x; i < arr.length; i++) {
+  arr[i] = 0;
+}
+
+console.log(arr)
