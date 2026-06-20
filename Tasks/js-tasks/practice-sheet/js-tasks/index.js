7aa667a15972398abc3e0149e0fbf87e56e1bcc8
Nofal Ali <nofilkachhot@gmail.com>
2026-06-20 14:21:10 +0530

    solved 2 q

---
+++ Tasks/js-tasks/practice-sheet/js-tasks/index.js
@@ -0,0 +1,16 @@
+// 1. Even or Odd Checker.
+
+function checkEvenOdd() {
+  let n = 9;
+  return n % 2 === 0 ? "Even" : "odd";
+}
+// let ans = checkEvenOdd();
+// console.log(ans);
+
+// 2. Greeting Generator.
+
+function greet(name, age) {
+  return `Hello, ${name}! You are ${age} years old`;
+}
+let ans = greet("Nofal", 22);
+console.log(ans)
