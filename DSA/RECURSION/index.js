7fbcb2fb220994b8894213c1c3e26bac46a5a99a
Nofal Ali <nofilkachhot@gmail.com>
2026-06-23 15:40:57 +0530

    solved 2 problems on recursion

--- DSA/RECURSION/index.js
+++ DSA/RECURSION/index.js
@@ -4,4 +4,26 @@ function sum(n) {
   if (n === 0) return 0;
   return n + sum(n - 1);
 }
-console.log(sum(5));
+// console.log(sum(5));
+
+// 2. sum of all numbers in array.
+
+let arr = [5, 3, 2, 0, 1, 4];
+function sum(n) {
+  if (n === 0) return arr[n];
+
+  return arr[n] + sum(n - 1);
+}
+// console.log(sum(arr.length - 1));
+
+// 3. sum of all odd numbers in array.
+
+function sum(n) {
+  let isOdd = arr[n] % 2 !== 0;
+
+  if (n === 0) {
+    return isOdd ? arr[n] : 0;
+  }
+  return (isOdd ? arr[n] : 0) + sum(n - 1);
+}
+console.log(sum(arr.length - 1))
