38c0dc1fc72c38d5d62dce50daf381698d72ca16
Nofal Ali <nofilkachhot@gmail.com>
2026-06-30 23:35:16 +0530

    bubble sort revison

--- JS/RANDOM_PRACTICE/index.js
+++ JS/RANDOM_PRACTICE/index.js
@@ -243,7 +243,21 @@ function binary(nums, target) {
 
 // binary search.
 
-// function binarySearch() {
-
-// }
-// console.log(binarySearch());
+let arr2 = [5,4,2,1]
+function bubbleSort(arr) {
+  let n = arr.length;
+      for(let i=0; i<n-1; i++){
+        let isSwapped = false;
+        for(let j=0; j<n-1-i; j++){
+          if(arr[j] > arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+            isSwapped = true
+          }
+        }
+        if(!isSwapped) break;
+      }
+      return arr
+}
+console.log(bubbleSort(arr2));
