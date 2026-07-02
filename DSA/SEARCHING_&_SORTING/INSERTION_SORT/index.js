033a98ea25ebe2d2db9582bf9a6e9c61545fb39d
Nofal Ali <nofilkachhot@gmail.com>
2026-07-02 11:53:58 +0530

    insertion sort q solved

--- DSA/SEARCHING_&_SORTING/INSERTION_SORT/index.js
+++ DSA/SEARCHING_&_SORTING/INSERTION_SORT/index.js
@@ -1,5 +1,17 @@
-let arr = [7,4,3,5,1,2];
+let arr = [7, 4, 3, 5, 1, 2];
 function insertionSort(a) {
-    let n = a.length
+  let n = a.length;
+
+  for (let i = 1; i < n; i++) {
+    let curr = a[i];
+    let prev = i - 1;
+
+    while (prev >= 0 && a[prev] > curr) {
+      a[prev + 1] = a[prev];
+      prev--;
+    }
+    a[prev + 1] = curr;
+  }
+  return a
 }
 console.log(insertionSort(arr));
