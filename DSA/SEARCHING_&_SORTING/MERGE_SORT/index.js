1f339462ff7508a66fe9cda6ea28055e58a23050
Nofal Ali <nofilkachhot@gmail.com>
2026-07-04 23:12:33 +0530

    random practice

--- DSA/SEARCHING_&_SORTING/MERGE_SORT/index.js
+++ DSA/SEARCHING_&_SORTING/MERGE_SORT/index.js
@@ -17,12 +17,12 @@ function merge(left, right) {
   return [...res, ...left.slice(i), ...right.slice(j)];
 }
 
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
+// function mergeSort(arr) {
+//   if (arr.length <= 1) return arr;
 
-  let mid = Math.floor(arr.length / 2);
-  let left = mergeSort(arr.slice(0, mid));
-  let right = mergeSort(arr.slice(mid));
-  return merge(left, right);
-}
+//   let mid = Math.floor(arr.length / 2);
+//   let left = mergeSort(arr.slice(0, mid));
+//   let right = mergeSort(arr.slice(mid));
+//   return merge(left, right);
+// }
 // console.log(mergeSort(arr));
