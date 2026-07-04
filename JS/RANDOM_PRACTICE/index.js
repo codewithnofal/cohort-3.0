61d0e95dd4939face33899a9b09ad2cf89bc756a
Nofal Ali <nofilkachhot@gmail.com>
2026-07-04 12:28:27 +0530

    dsa sorting practice

--- JS/RANDOM_PRACTICE/index.js
+++ JS/RANDOM_PRACTICE/index.js
@@ -278,9 +278,27 @@ function selectionSort(arr) {
     arr[min] = arr[i];
     arr[i] = temp;
   }
-  return arr
+  return arr;
 }
 // console.log(selectionSort(arr3));
 
+// bubble sort
 
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let isSwapped = false;
 
+    for (let j = 0; j < n - 1-i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        isSwapped = true;
+      }
+    }
+    if (!isSwapped) break;
+  }
+  return arr;
+}
+console.log(bubbleSort(arr3));
