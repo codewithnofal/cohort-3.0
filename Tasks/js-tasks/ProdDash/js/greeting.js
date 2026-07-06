4bcc63c4e877fdfe478bd452bb7531fa05083412
Nofal Ali <nofilkachhot@gmail.com>
2026-07-06 15:22:23 +0530

    greeting logic

--- Tasks/js-tasks/ProdDash/js/greeting.js
+++ Tasks/js-tasks/ProdDash/js/greeting.js
@@ -1,6 +1,22 @@
-const greeting = document.querySelector("#greeting")
+const greeting = document.querySelector("#greeting");
 const greetingMessage = document.querySelector("#greeting-message");
 
-function updateGreeting(){
-    
+function updateGreeting() {
+  const hour = new Date().getHours();
+
+  if (hour >= 5 && hour < 12) {
+    greeting.textContent = "Good Morning, ☀️";
+    greetingMessage.textContent =
+      "Rise and shine! Let's make today productive.";
+  } else if (hour >= 12 && hour < 17) {
+    greeting.textContent = "Good Afternoon, 🌤";
+    greetingMessage.textContent = "Keep pushing! You're doing great.";
+  } else if (hour >= 17 && hour < 20) {
+    greeting.textContent = "Good Evening, 🌇";
+    greetingMessage.textContent = "Stay focused. Finish what you started.";
+  } else {
+    greeting.textContent = "Good Night, 🌙";
+    greetingMessage.textContent = "Wrap up your day and prepare for tomorrow.";
+  }
 }
+updateGreeting();
