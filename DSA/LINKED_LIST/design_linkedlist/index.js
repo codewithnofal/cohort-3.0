634ebd9a016fb0bf84d422a3cb55c41da87119b2
Nofal Ali <nofilkachhot@gmail.com>
2026-07-09 23:30:40 +0530

    changes apply

--- DSA/LINKED_LIST/design_linkedlist/index.js
+++ DSA/LINKED_LIST/design_linkedlist/index.js
@@ -17,32 +17,3 @@ myLinkedList.prototype.addAtHead = (val) => {
   this.size++;
 };
 
-myLinkedList.prototype.addAtTail = (val) => {
-  let newNode = new Node(val);
-  if (this.head === null) {
-    this.head = newNode;
-  } else {
-    let curr = this.head;
-    while (curr.next !== null) {
-      curr = curr.next;
-    }
-    curr.next = newNode;
-  }
-  this.size++;
-};
-
-myLinkedList.prototype.addAtIndex = (index, val) => {
-  let newNode = new Node(val);
-
-  if (index < 0 || index > this.size) return;
-
-  let curr = this.head;
-
-  for (let i = 0; i < index - 1; i++) {
-    curr = curr.next;
-  }
-  newNode.next = curr.next;
-  curr.next = newNode;
-  this.size++;
-};
-
