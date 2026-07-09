afaf4ecb657559404ec227c4bbac7db7fd996182
Nofal Ali <nofilkachhot@gmail.com>
2026-07-09 23:29:50 +0530

    attempt to linkedlist

--- DSA/LINKED_LIST/design_linkedlist/index.js
+++ DSA/LINKED_LIST/design_linkedlist/index.js
@@ -1,4 +1,48 @@
-function Node(val){
+class Node {
+  constructor(val) {
     this.val = val;
     this.next = null;
+  }
 }
+
+let myLinkedList = () => {
+  this.head = null;
+  this.size = 0;
+};
+
+myLinkedList.prototype.addAtHead = (val) => {
+  let newNode = new Node(val);
+  newNode.next = this.head;
+  this.head = newNode;
+  this.size++;
+};
+
+myLinkedList.prototype.addAtTail = (val) => {
+  let newNode = new Node(val);
+  if (this.head === null) {
+    this.head = newNode;
+  } else {
+    let curr = this.head;
+    while (curr.next !== null) {
+      curr = curr.next;
+    }
+    curr.next = newNode;
+  }
+  this.size++;
+};
+
+myLinkedList.prototype.addAtIndex = (index, val) => {
+  let newNode = new Node(val);
+
+  if (index < 0 || index > this.size) return;
+
+  let curr = this.head;
+
+  for (let i = 0; i < index - 1; i++) {
+    curr = curr.next;
+  }
+  newNode.next = curr.next;
+  curr.next = newNode;
+  this.size++;
+};
+
