4f88966126262916257690f4e5bd25c9d535f843
Nofal Ali <nofilkachhot@gmail.com>
2026-07-12 00:18:26 +0530

    validation in RHF and create and read

--- react/RHF/src/components/UserForm.jsx
+++ react/RHF/src/components/UserForm.jsx
@@ -3,73 +3,106 @@ import { useRef } from "react";
 import { useForm } from "react-hook-form";
 
 function UserForm() {
-    
   let {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    mode: "onChange"
+  });
 
   const formSubmit = (data) => {
     console.log(data);
-    reset()
+    reset();
   };
 
   return (
-    <div className="w-full max-w-lg rounded-2xl bg-white p-8 shadow-xl">
-      <h2 className="mb-6 text-center text-3xl font-bold text-slate-800">
+    <div className="w-full max-w-lg h-[90%] rounded-2xl bg-white p-8 shadow-xl">
+      <h2 className="mb-3 text-center text-3xl font-bold text-slate-800">
         Add User
       </h2>
 
-      <form onSubmit={handleSubmit(formSubmit)} className="space-y-5">
+      <form onSubmit={handleSubmit(formSubmit)} className="space-y-3">
         <div>
           <label className="mb-2 block text-sm font-medium text-slate-700">
             Name
           </label>
           <input
-            {...register("userName")}
+            {...register("userName", {
+              required: "name is required",
+            })}
             type="text"
             placeholder="Enter your name"
             className="w-full rounded-xl border border-gray-300 px-4 py-3 outline-none transition focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
           />
         </div>
+        {errors.userName && (
+          <p className="text-red-600">{errors.userName.message}</p>
+        )}
 
         <div>
           <label className="mb-2 block text-sm font-medium text-slate-700">
             Email
           </label>
           <input
-            {...register("userEmail")}
+            {...register("userEmail", {
+              required: "email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "please enter valid email"
+              }
+            })}
             type="email"
             placeholder="Enter your email"
             className="w-full rounded-xl border border-gray-300 px-4 py-3 outline-none transition focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
           />
         </div>
+        {errors.userEmail && (
+          <p className="text-red-600">{errors.userEmail.message}</p>
+        )}
 
         <div>
           <label className="mb-2 block text-sm font-medium text-slate-700">
             Password
           </label>
           <input
-            {...register("userPassword")}
+            {...register("userPassword", {
+              required: "password is required",
+              minLength: {
+                value: 10,
+                message: "min 10 digits are required",
+              },
+              maxLength: {
+                value: 10,
+                message: "max 10 digits are required",
+              },
+            })}
             type="password"
             placeholder="Enter your password"
             className="w-full rounded-xl border border-gray-300 px-4 py-3 outline-none transition focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
           />
         </div>
+        {errors.userPassword && (
+          <p className="text-red-600">{errors.userPassword.message}</p>
+        )}
 
         <div>
           <label className="mb-2 block text-sm font-medium text-slate-700">
             Image URL
           </label>
           <input
-            {...register("userImage")}
+            {...register("userImage", {
+              required: "image is required",
+            })}
             type="text"
             placeholder="Paste image URL"
             className="w-full rounded-xl border border-gray-300 px-4 py-3 outline-none transition focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
           />
         </div>
+        {errors.userImage && (
+          <p className="text-red-600">{errors.userImage.message}</p>
+        )}
 
         <button
           type="submit"
