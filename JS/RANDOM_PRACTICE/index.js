d2afedebad96c22f999fc0775d856c9a7373907a
Nofal Ali <nofilkachhot@gmail.com>
2026-07-16 12:51:10 +0530

    bubble sort revison

--- JS/RANDOM_PRACTICE/index.js
+++ JS/RANDOM_PRACTICE/index.js
@@ -284,12 +284,33 @@ function selectionSort(arr) {
 
 // bubble sort
 
+// function bubbleSort(arr) {
+//   let n = arr.length;
+//   for (let i = 0; i < n - 1; i++) {
+//     let isSwapped = false;
+
+//     for (let j = 0; j < n - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//         isSwapped = true;
+//       }
+//     }
+//     if (!isSwapped) break;
+//   }
+//   return arr;
+// }
+// console.log(bubbleSort(arr3));
+
+// rivison of bubble sort;
+let arr6 = [6, 5, 3, 4, 1, 2];
 function bubbleSort(arr) {
   let n = arr.length;
+
   for (let i = 0; i < n - 1; i++) {
     let isSwapped = false;
-
-    for (let j = 0; j < n - 1-i; j++) {
+    for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -301,4 +322,4 @@ function bubbleSort(arr) {
   }
   return arr;
 }
-console.log(bubbleSort(arr3));
+// console.log(bubbleSort(arr6));
