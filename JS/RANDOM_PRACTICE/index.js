9419f264b6b369dd8cbdd3f4cadae4ee8a45bf8b
Nofal Ali <nofilkachhot@gmail.com>
2026-07-16 13:16:48 +0530

    revison of selection sort

--- JS/RANDOM_PRACTICE/index.js
+++ JS/RANDOM_PRACTICE/index.js
@@ -323,3 +323,23 @@ function bubbleSort(arr) {
   return arr;
 }
 // console.log(bubbleSort(arr6));
+
+// selection sort revison.
+
+function selectionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+  return arr;
+}
+// console.log(selectionSort(arr6));
