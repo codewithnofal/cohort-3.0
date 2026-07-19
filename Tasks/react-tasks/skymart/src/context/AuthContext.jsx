31e14276334c6662b7357f21faabcc104397befa
Nofal Ali <nofilkachhot@gmail.com>
2026-07-19 23:11:24 +0530

    set local storage

--- Tasks/react-tasks/skymart/src/context/AuthContext.jsx
+++ Tasks/react-tasks/skymart/src/context/AuthContext.jsx
@@ -4,6 +4,7 @@ export const AuthStore = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
+  localStorage.setItem("users", JSON.stringify(users));
   console.log(users);
   return (
     <AuthStore.Provider value={{ users, setUsers }}>
