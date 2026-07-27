417e5b94bca7e0464e13f952681cbd7eab62120c
Nofal Ali <nofilkachhot@gmail.com>
2026-07-27 23:25:37 +0530

    complete implementation of redux

---
+++ react/redux-toolkit/src/features/counterSlice.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name:"count",
+    initialState:{
+        count:1
+    },
+    reducers:{
+        increment: (state) => {
+            state.count++;
+        },
+        decrement: (state) => {
+            state.count--;
+        }
+    }
+})
+
+export const {increment,decrement} = counterSlice.actions;
+
+export default counterSlice.reducer;
