1fe36e3aa1d8333b9dca00dbd94a50a32c1e696b
Nofal Ali <nofilkachhot@gmail.com>
2026-07-30 21:55:52 +0530

    revise

--- react/redux-toolkit/src/main.jsx
+++ react/redux-toolkit/src/main.jsx
@@ -5,8 +5,4 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
 
-createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-);
+createRoot(document.getElementById("root")).render(<App />);
