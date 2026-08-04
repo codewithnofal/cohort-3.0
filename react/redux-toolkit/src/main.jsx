7c4c891487b38b91904ac26d6bf292ffc43f3515
Nofal Ali <nofilkachhot@gmail.com>
2026-08-04 21:20:37 +0530

    update changes

--- react/redux-toolkit/src/main.jsx
+++ react/redux-toolkit/src/main.jsx
@@ -5,4 +5,8 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+);
