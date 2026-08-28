dcb021623e7a551ce62b69adce80a7d2c06af448
Nofal Ali <nofilkachhot@gmail.com>
2026-08-28 23:22:09 +0530

    Add spacing and comments for clarity in server.js

--- backend/create-server/server.js
+++ backend/create-server/server.js
@@ -1,10 +1,16 @@
+// esm 
 let http = require('http');
 
+
+// creating a server
 let server = http.createServer((req,res) => {
     res.end("server running successfully...")
 })
 
+// listen to a server
 server.listen(3000, () => {
     console.log("server is running on port 3000")
 })
+
+
 
