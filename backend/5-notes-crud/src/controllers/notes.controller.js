c6f4f61843c9faf19e24a249daaa3dbf2f344e40
Nofal Ali <nofilkachhot@gmail.com>
2026-08-31 12:03:41 +0530

    Add CRUD operations for notes: create, read all, read single, update, and delete

--- backend/5-notes-crud/src/controllers/notes.controller.js
+++ backend/5-notes-crud/src/controllers/notes.controller.js
@@ -1,5 +1,6 @@
 const NotesModel = require("../models/notes.model");
 
+// CREATE
 const createNotesController = async (req, res) => {
   let { title, description } = req.body;
 
@@ -14,6 +15,73 @@ const createNotesController = async (req, res) => {
   });
 };
 
+// READ ALL NOTES
+const getAllNotesController = async (req, res) => {
+  try {
+    const getAllNotes = await NotesModel.find();
+
+    return res.status(200).json({
+      message: "all notes fetched",
+      data: getAllNotes,
+    });
+  } catch (error) {
+    return res.status(500).json("internal server error");
+  }
+};
+
+// READ SINGLE NOTE
+const getSingleNoteController = async (req, res) => {
+  let noteId = req.params.id;
+
+  try {
+    const singleNote = await NotesModel.findById(noteId);
+
+    return res.status(200).json({
+      message: "fetched single note successfully",
+      data: singleNote,
+    });
+  } catch (error) {
+    return res.status(500).json("internal server error");
+  }
+};
+
+
+// UPDATE
+const updateNotesController = async (req, res) => {
+  try {
+    let noteId = req.params.id;
+    let data = req.body;
+
+    const notesUpdate = await NotesModel.findByIdAndUpdate(noteId, data, {new: true});
+
+    return res.status(200).json({
+      message: "note updated successfully",
+      data: notesUpdate,
+    });
+  } catch (error) {
+    return res.status(500).json("internal server error");
+  }
+};
+
+// DELETE
+const deleteNoteController = async (req, res) => {
+    try {
+        let noteId = req.params.id;
+        const deleteNote = await NotesModel.findByIdAndDelete(noteId);
+
+        return res.status(200).json({
+            message: "Note deleted successfully",
+            data: deleteNote
+        })
+    } catch (error) {
+        return res.status(500).json("internal server error")
+    }
+}
+
 module.exports = {
   createNotesController,
+  getAllNotesController,
+  getSingleNoteController,
+  updateNotesController,
+  deleteNoteController
 };
--- backend/5-notes-crud/src/routes/notes.route.js
+++ backend/5-notes-crud/src/routes/notes.route.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { createNotesController } = require("../controllers/notes.controller");
+const {
+  createNotesController,
+  getAllNotesController,
+  getSingleNoteController,
+  updateNotesController,
+  deleteNoteController,
+} = require("../controllers/notes.controller");
 
 const router = express.Router();
 
 router.post("/create", createNotesController);
+router.get("/getAllNotes", getAllNotesController);
+router.get("/:id", getSingleNoteController);
+router.put('/:id', updateNotesController)
+router.delete('/:id', deleteNoteController)
 
 module.exports = router;
