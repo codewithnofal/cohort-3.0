e345cdc10fd2c095816c0e8f1d8ccc47184900b9
Nofal Ali <nofilkachhot@gmail.com>
2026-09-05 18:19:11 +0530

    feat: initialize authentication backend with Express and dotenv
    - Added package.json for project dependencies and scripts.
    - Created server.js to set up the server and listen on port 3000.
    - Implemented app.js to configure the Express application.

---
+++ backend/9-authantication/server.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./src/app.js";
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
