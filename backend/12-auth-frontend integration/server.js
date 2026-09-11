7607d0a0aea10689c5c1adc970ee9a518f4e0b55
Nofal Ali <nofilkachhot@gmail.com>
2026-09-11 23:06:30 +0530

    feat: set up database connection and implement user model with authentication routes

--- backend/12-auth-frontend integration/server.js
+++ backend/12-auth-frontend integration/server.js
@@ -1,5 +1,8 @@
 import app from "./src/app/app.js";
+import connectDB from "./src/config/db.config.js";
+
+await connectDB();
 
 app.listen(3000, () => {
-    console.log(`server is running on port 3000`)
-})
+  console.log(`server is running on port 3000`);
+});
